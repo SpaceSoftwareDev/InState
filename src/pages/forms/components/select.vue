bc31cae4fa50427c0c475efe37b99de61ef013ae
Samuel Uhliar <uhliar@wezeo.com>
2023-06-24 03:06:32 +0200

    .

---
+++ src/pages/forms/components/select.vue
@@ -0,0 +1,52 @@
+<template>
+	<h1>Na aký zámer by ste ju chceli zmeniť?</h1>
+	<div class="holder">
+		<select>
+			<option v-for="option in options" :key="option" :value="option">{{option}}</option>
+		</select>
+		<button>Vrátiť sa naspať</button>
+	</div>
+</template>
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+
+const options = [
+	"Gymnázium",
+	"Elektrotechnická",
+	"Strojnícka",
+	"Ekonomická",
+]
+</script>
+<style lang="scss" scoped>
+h1 {
+	text-align: center;
+}
+
+button {
+	@apply bg-white text-blueish border-blueish;
+	border: 1px solid;
+	border-radius: 2px;
+	font-size: 1em;
+}
+
+.holder {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: center;
+}
+.holder * {
+	display: inline-flex;
+	margin: 10px;
+}
+
+select {
+	border: 1px solid #E7E7E9;
+	border-radius: 2px;
+	background: rgb(251,251,251);
+	background: linear-gradient(150deg, rgba(251,251,251,1) 23%, rgba(242,242,242,1) 85%);
+	width: 200px;
+	height: 55px;
+	@apply  text-blueish;
+}
+</style>
--- src/pages/moving/+Page.vue
+++ src/pages/moving/+Page.vue
@@ -1,72 +1,11 @@
 <template>
-	<form class="mt-10" @submit.prevent="submit">
-		<span class="text-lg font-bold" v-text="stepper.current.value.title" />
-		<div class="flex flex-col justify-center gap-2 mt-2">
-			<div>
-			<div v-if="stepper.isCurrent('mou-permission')">
-				<img :src="icons.ArrowRight" class="icon" />
-				<span>{{ stepper.current.value.description }}</span>
-				<button class="mou">Import Data from Mou</button>
-			</div>
-			<div v-if="stepper.isCurrent('user-information')">
-				<span>First name:</span>
-				<input v-model="form.firstName" class="!mt-0.5" type="text">
-				<span>Last name:</span>
-				<input v-model="form.lastName" class="!mt-0.5" type="text">
-			</div>
-		</div>
+	<Select>
 
-		<div v-if="!stepper.isCurrent('mou-permission')">
-			<button v-if="!stepper.isLast.value" :disabled="!stepper.current.value.isValid()">
-				Next
-			</button>
-			<button v-if="stepper.isLast.value" :disabled="!stepper.current.value.isValid()">
-				Submit
-			</button>
-		</div>
-		</div>
-	</form>
+	</Select>
 </template>
 
 <script lang="ts" setup>
-import * as icons from "../../icons"
-import { useStepper } from '@vueuse/core'
-import { reactive } from 'vue'
-
-const form = reactive({
-	firstName: 'Jon',
-	lastName: '',
-	billingAddress: '',
-	contractAccepted: false,
-	carbonOffsetting: false,
-	payment: 'credit-card' as 'paypal' | 'credit-card',
-})
-
-const stepper = useStepper({
-	'mou-permission': {
-		icon: icons.Upload,
-		title: 'Poďme importovať dáta',
-		description: 'Po kliknutí na tlačítko nižšie vám budú importované vaše osobné údaje ako napr meno, email, telefónne číslo, bydlisko zo štátnej datábazy MOU (Manažment osobných údajov)',
-		isValid: () => form.firstName && form.lastName,
-	},
-	'user-information': {
-		icon: icons.Upload,
-		title: 'User information',
-		description: 'Po kliknutí na tlačítko nižšie vám budú importované vaše osobné údaje ako napr meno, email, telefónne číslo, bydlisko zo štátnej datábazy MOU (Manažment osobných údajov)',
-		isValid: () => form.firstName && form.lastName,
-	}
-})
-
-function submit() {
-if (stepper.current.value.isValid())
-	stepper.goToNext()
-}
-
-function allStepsBeforeAreValid(index: number): boolean {
-return !Array(index)
-	.fill(null)
-	.some((_, i) => !stepper.at(i)?.isValid())
-}
+import Select from "../forms/components/select.vue"
 </script>
 
 <style lang="scss" scoped>
